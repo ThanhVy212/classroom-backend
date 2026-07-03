e3e7ce05d66e621c507970c4481a53d1d8413e81
Thanh Vỵ <buivy22122004@gmail.com>
2026-07-04 01:54:27 +0700

    change status code when many request to 429

--- src/middleware/security.ts
+++ src/middleware/security.ts
@@ -54,7 +54,7 @@ const securityMiddleware = async (req: Request, res: Response, next: NextFunctio
         }
 
         if(decision.isDenied() && decision.reason.isRateLimit()) {
-            return res.status(403).json({error: 'Too many requests', message: message});
+            return res.status(429).json({error: 'Too many requests', message: message});
         }
 
         next();
